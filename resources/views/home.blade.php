3dd04945781a716de4e76e2c1e31655a814adf71
dimascahyo557 <dimasamid557@gmail.com>
2023-02-28 10:47:04 +0700

    make home page view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.home_layout')
+
+@section('title', 'Beranda')
+
+@section('content')
+    <div class="d-grid">
+        <img class="m-auto" src="{{ asset('image/alquran.png') }}" alt="gambar alquran" style="width: 50%;">
+        <div class="btn btn-outline-secondary border-2 mb-3">Baca Al-Quran</div>
+        <div class="btn btn-outline-secondary border-2 mb-3">Terakhir Baca</div>
+        <div class="btn btn-outline-secondary border-2 mb-3">Tentang Aplikasi</div>
+    </div>
+@endsection
+
+@section('credit')
+    Image by <a href="https://www.freepik.com/free-vector/hand-drawn-tasbih-illustration_22432656.htm#query=alquran&position=0&from_view=search&track=sph">Freepik</a>
+@endsection
---
+++ resources/views/layouts/home_layout.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Al-Quranku | @yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
+    <link rel="shortcut icon" href="{{ asset('image/alquran.png') }}" type="image/x-icon">
+
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+        #footer {
+            justify-self: flex-end;
+        }
+    </style>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand m-auto" href="{{ url('/') }}">Al-Quranku</a>
+        </div>
+    </nav>
+    <nav class="navbar navbar-expand bg-body-tertiary position-sticky top-0">
+        <div class="container-fluid">
+            <div class="navbar-nav m-auto">
+                <a class="nav-link" href="{{ url('/') }}">Beranda</a>
+                <a class="nav-link" href="#">Baca per juz</a>
+                <a class="nav-link" href="#">Baca per surah</a>
+            </div>
+        </div>
+    </nav>
+    
+    <main class="m-auto" style="max-width: 600px;">
+        <div class="container my-3">
+            @yield('content')
+        </div>
+    </main>
+
+    <footer class="navbar navbar-expand-lg bg-body-tertiary" id="footer">
+        <div class="container-fluid">
+            <div class="mx-auto my-3 text-center">
+                <div class="text-muted">
+                    &copy; 2023 Al-Quranku
+                </div>
+                <small>
+                    @yield('credit')
+                </small>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
