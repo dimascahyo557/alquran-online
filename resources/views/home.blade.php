2f6f6e74f86c8bb51ce9a33eb5cf236b970ebb2e
dimascahyo557 <dimasamid557@gmail.com>
2023-03-03 17:01:36 +0700

    add error message in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,15 @@
 @section('content')
     <div class="d-grid">
         <img class="m-auto" src="{{ asset('image/alquran.png') }}" alt="gambar alquran" style="width: 50%;">
+
+        {{-- Error Message --}}
+        @if (session()->has('message'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ session('message') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+
         <a href="{{ route('surat.list') }}" class="btn btn-outline-secondary border-2 mb-3">Baca Al-Quran</a>
         <a href="{{ $lastReadUrl }}" class="btn btn-outline-secondary border-2 mb-3">Terakhir Baca</a>
         <a href="{{ route('about-app') }}" class="btn btn-outline-secondary border-2 mb-3">Tentang Aplikasi</a>
